ca42daf14bf19501f403bc9822696faa4de58fd7
aliali <aliali@test.com>
2025-05-08 09:10:50 +0300

    CRUD endpoints

--- src/main/java/com/example/final_project/Controller/AuditorController.java
+++ src/main/java/com/example/final_project/Controller/AuditorController.java
@@ -1,4 +1,42 @@
 package com.example.final_project.Controller;
 
+import com.example.final_project.Api.ApiResponse;
+import com.example.final_project.DTO.DTOAuditor;
+import com.example.final_project.Service.AuditorService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/auditor")
+@RequiredArgsConstructor
 public class AuditorController {
+
+    private final AuditorService auditorService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAll(){
+        return ResponseEntity.status(200).body(auditorService.getAllAuditors());
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity addBranch(@RequestBody@Valid DTOAuditor dtoAuditor){
+        auditorService.addAuditor(dtoAuditor);
+        return ResponseEntity.status(200).body(new ApiResponse("new auditor added"));
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity updateAuditor(@PathVariable Integer id,@RequestBody@Valid DTOAuditor dtoAuditor){
+        auditorService.updateAuditor(id, dtoAuditor);
+        return ResponseEntity.status(200).body(new ApiResponse("auditor updated"));
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity deleteAuditor(@PathVariable Integer id){
+        auditorService.deleteAuditor(id);
+        return ResponseEntity.status(200).body(new ApiResponse("auditor deleted"));
+    }
+
+
 }
