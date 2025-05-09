848a1ffe6503d6755d909a50d4f441de2b3d1c58
aliali <aliali@test.com>
2025-05-09 18:10:42 +0300

    restock product 35

--- src/main/java/com/example/final_project/Controller/AccountantController.java
+++ src/main/java/com/example/final_project/Controller/AccountantController.java
@@ -1,8 +1,12 @@
 package com.example.final_project.Controller;
 
 
+import com.example.final_project.Api.ApiResponse;
 import com.example.final_project.Service.AccountantService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountantController {
 
     private final AccountantService accountantService;
+
+    @PutMapping("/restock-product/accountant/{accountantId}/product/{productId}/amount/{amount}")
+    public ResponseEntity restockProduct(@PathVariable Integer accountantId,@PathVariable Integer productId,@PathVariable Integer amount){
+        accountantService.restockProduct(accountantId, productId, amount);
+        return ResponseEntity.status(200).body(new ApiResponse("product restocked successfully"));
+    }
+
 }
--- src/main/java/com/example/final_project/Service/AccountantService.java
+++ src/main/java/com/example/final_project/Service/AccountantService.java
@@ -1,6 +1,10 @@
 package com.example.final_project.Service;
 
+import com.example.final_project.Api.ApiException;
+import com.example.final_project.Model.Accountant;
+import com.example.final_project.Model.Product;
 import com.example.final_project.Repository.AccountantRepository;
+import com.example.final_project.Repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +13,19 @@ import org.springframework.stereotype.Service;
 public class AccountantService {
 
     private final AccountantRepository accountantRepository;
+    private final ProductRepository productRepository;
+
+    // Endpoint 35
+    public void restockProduct(Integer accountantId, Integer productId, Integer amount){
+        Accountant accountant = accountantRepository.findAccountantById(accountantId);
+        Product product = productRepository.findProductById(productId);
+        if (accountant==null)
+            throw new ApiException("accountant not found");
+        if (product==null)
+            throw new ApiException("product not found");
+        product.setStock(product.getStock()+amount);
+
+        productRepository.save(product);
+    }
+
 }
