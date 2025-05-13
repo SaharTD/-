cde02237692b27f7983bb94f4a99d49695e366cf
aliali <aliali@test.com>
2025-05-13 12:35:40 +0300

    updates

--- src/main/java/com/example/final_project/Config/ConfigurationSecurity.java
+++ src/main/java/com/example/final_project/Config/ConfigurationSecurity.java
@@ -20,9 +20,9 @@ public class ConfigurationSecurity {
     private final MyUserDetailsService myUserDetailsService;
 
     @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(){
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
 
-        DaoAuthenticationProvider authenticationProvider=new DaoAuthenticationProvider();
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
         authenticationProvider.setUserDetailsService(myUserDetailsService);
         authenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder());
 
@@ -42,32 +42,25 @@ public class ConfigurationSecurity {
                 .requestMatchers("/api/v1/business/add-business",
                         "/api/v1/branch/add/",
                         "api/v1/tax-payer/add-accountant/",
-                        "api/v1/accountant/assign-accountant-to-branch/accountant/").hasAuthority("TAXPAYER")
-                .requestMatchers("/api/v1/product/add/").hasAuthority("ACCOUNTANT")
-                //   AUDITOR tax report
-//                .requestMatchers(
-//                        "/api/v1/taxReports/add",
-//                        "/api/v1/taxReports/get",
-//                        "/api/v1/taxReports/update",
-//                        "/api/v1/taxReports/delete")
-//                .hasAuthority("AUDITOR")
-//
-//                .requestMatchers( "/api/v1/taxReports/apply-penalty/**",
-//                        "/api/v1/taxReports/apply-2month-penalty/**",
-//                        "/api/v1/taxReports/apply-legal-action/**",
-//                        "/api/v1/taxReports/due-payment",
-//                        "/api/v1/taxReports/reports/**",
-//                        "/api/v1/taxReports/report-count/**",
-//                        "/api/v1/taxReports/approval-rate/**",
-//                        "/api/v1/taxReports/bulk-approve/**",
-//                        "/api/v1/taxReports/latest-report",
-//                        "/api/v1/taxReports/unapproved",
-//                        "/api/v1/taxReports/*/payment-status",
-//                        "/api/v1/auditor/create-tax-report/**",
-//                        "/api/v1/auditor/activate-business/**",
-//                        "/api/v1/auditor/approve-tax-report/**",
-//                        "/api/v1/auditor/reject-tax-report/**"
-//                ).hasRole("AUDITOR")
+                        "api/v1/accountant/assign-accountant-to-branch/accountant/")
+                .hasAuthority("TAXPAYER")
+                .requestMatchers("/api/v1/product/add/", "/api/v1/accountant/get-accountant-by-branch/{{branchId}}"
+                        , "/api/v1/accountant/get-accountant-by-business/{{businessId}}"
+                        , "/api/v1/accountant/assign-accountant-to-branch/accountant/{{accountantId}}/{{branchId}}"
+                        , "/api/v1/accountant/update/{{id}}", "/api/v1/accountant/delete/{{id}}"
+                        , "/api/v1/accountant/restock-product/accountant/{{accountantId}}/product/{{productId}}/amount/{{amount}}")
+                .hasAuthority("ACCOUNTANT")
+                .requestMatchers(
+                        "/api/v1/taxReports/add","/api/v1/taxReports/get","/api/v1/taxReports/update",
+                        "/api/v1/taxReports/delete","/api/v1/taxReports/apply-penalty/**",
+                        "/api/v1/taxReports/apply-2month-penalty/**","/api/v1/taxReports/apply-legal-action/**",
+                        "/api/v1/taxReports/due-payment","/api/v1/taxReports/reports/**",
+                        "/api/v1/taxReports/report-count/**","/api/v1/taxReports/approval-rate/**",
+                        "/api/v1/taxReports/bulk-approve/**","/api/v1/taxReports/latest-report",
+                        "/api/v1/taxReports/unapproved","/api/v1/taxReports/*/payment-status",
+                        "/api/v1/auditor/create-tax-report/**","/api/v1/auditor/activate-business/**",
+                        "/api/v1/auditor/approve-tax-report/**","/api/v1/auditor/reject-tax-report/**"
+                ).hasAuthority("AUDITOR")
                 .anyRequest().authenticated()
                 .and()
                 .logout()
